6edaf5b173e61d435e13e8ff34502b00c5918efa
scanf6 <scanf555@gmail.com>
2020-11-12 00:06:33 +0300

    starting Jotto app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Counter from './components/Counter';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div data-test='appComponent'>
-        <Counter />
+        Jotto
       </div>
     );
   }
