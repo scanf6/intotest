735df2c905cb48d3ff917b104a41cc491fa41666
scanf6 <scanf555@gmail.com>
2020-11-12 00:41:21 +0300

    added test file

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "../App";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Jotto app', ()=>{});
