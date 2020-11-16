20459821ad1556379a8593b0a5e5628efad2633f
scanf6 <scanf555@gmail.com>
2020-11-16 17:17:01 +0300

    added test for successReducer

--- src/redux/reducers/successReducer.test.js
+++ src/redux/reducers/successReducer.test.js
@@ -2,6 +2,10 @@ import successReducer from './successReducer';
 import successActions from '../actions';
 
 describe('Success Reducer', ()=>{
+    it('Should return false by default if neither SUCCESS nor FAIL comes', ()=>{
+        const result = successReducer(undefined, {});
+        expect(result).toBe(false);
+    })
     it('Should return true if an action with type SUCCESS comes', ()=>{
         const result = successReducer(null, successActions.correctGuess());
         expect(result).toBe(true);
