f3f05ff838edba015bea8d0044e5dce14b5ae350
scanf6 <scanf555@gmail.com>
2021-01-13 18:05:15 +0300

    mistake in one test for testing purposes

--- src/__integrationTests__/index.test.js
+++ src/__integrationTests__/index.test.js
@@ -65,7 +65,7 @@ describe('Dispatching a SET SECRET WORD action', ()=>{
 
         return store.dispatch(actions.setSecretWord()).then(()=>{
             const newState = store.getState();
-            expect(newState.secretWord).toBe(secretWord)
+            expect(newState.secretWord).toBe(undefined)
         })
     });
 
